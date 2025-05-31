5e5f65fd56d81671601eb98b3cb503e55f1370de
Timofey <itimof3i@yandex.ru>
2025-05-31 14:26:47 +0500

    часть запросов в views

--- learning_log/strawberries/webappstraw/views.py
+++ learning_log/strawberries/webappstraw/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+def catalog(request):
+    return render(request, 'learning_log/strawberries/templates/main.html')
+
+def profile(request):
+    return render(request, '')
+
+def login(request):
+    return render(request, 'learning_log/strawberries/templates/login.html')
+
+def signin(request):
+    return render(request, 'learning_log/strawberries/templates/register.html')
 
